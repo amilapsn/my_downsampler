2233f8b80d7429d9ac8256860fcc303fb7438e0c
amilapsn <amilapsn@gmail.com>
2017-07-06 15:34:06 +0530

    init

---
+++ my_downsampler.srcs/sources_1/new/MAR.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/06/2017 02:13:48 PM
+// Design Name: 
+// Module Name: MAR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MAR(
+    input clk,
+    input [15:0] from_c,
+    output [15:0] to_a,
+    input enable
+    );
+endmodule
---
+++ my_downsampler.srcs/sources_1/new/custom_clock.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/06/2017 12:46:39 PM
+// Design Name: 
+// Module Name: custom_clock
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module custom_clock(
+    input in_clk,
+    output out_clk
+    );
+endmodule
